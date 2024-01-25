4f0f6bc1a9884ce14dded36e098137581818b3be
Juan Solis <juandsolism@gmail.com>
2024-01-24 18:06:31 -0600

    GnomeSort algorithm implementation

--- GnomeSort.java
+++ GnomeSort.java
@@ -7,9 +7,21 @@ public class GnomeSort<T extends Comparable<T>> implements ISort<T>{
 
     @Override
     public T[] sort(T[] arr) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sort'");
-    }
+        int index = 0;
 
-    
+        while (index < arr.length) {
+            if (index == 0) {
+                index++;
+            }
+            if (arr[index].compareTo(arr[index - 1]) >= 0) {
+                index++;
+            } else {
+                T temp = arr[index];
+                arr[index] = arr[index - 1];
+                arr[index - 1] = temp;
+                index--;
+            }
+        }
+        return arr;
+    }
 }
