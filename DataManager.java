adf761544026bb7e9dfc038989ab9df045183f17
Diego Flores <floresdiego041@gmail.com>
2024-01-27 22:07:13 -0600

    Creating random number generator method

--- DataManager.java
+++ DataManager.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Diego Flores & Juan Solis
  * @creationDate 24/01/2024
@@ -5,5 +8,20 @@
  */
 public class DataManager {
 
+    public int[] genRandomNumbers(int limit){
+        int[] myNumbers = new int[limit];
+        Set<Integer> savedNumbers = new HashSet<Integer>();
+        for (int i = 0; i < limit; i++) {
+            int randomNumber;
+            do {
+                randomNumber = (int)(Math.random()*15000) + 1;
+            } while (savedNumbers.contains(randomNumber));
+            myNumbers[i] = randomNumber;
+            savedNumbers.add(randomNumber);
+        }
+
+        return myNumbers;
+    }
+    
     
 }
