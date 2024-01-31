aa8b57bd9a78bef079b2b543d726b2e42d460686
Diego Flores <floresdiego041@gmail.com>
2024-01-30 21:55:31 -0600

    Fixing main menu and some bugs

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author Diego Flores & Juan Solis
  * @creationDate 24/01/2024
@@ -11,30 +13,131 @@ public class Main {
         QuickSort<Integer> quickSort = new QuickSort<>();
         RadixSort<Integer> radixSort = new RadixSort<>();
         InsertionSort<Integer> insertionSort = new InsertionSort<>();
+        Scanner sc = new Scanner(System.in);
 
         try {
             Integer[] randomNumbers = dataManager.genRandomNumbers(15000);
             dataManager.write(randomNumbers);
 
-            int limit = 10;
-            while (limit <= 3010) {
-                Integer[] dataCopy = dataManager.read(limit).clone();
-                gnomeSort.sort(dataCopy);
-            
-                Integer[] dataCopyMerge = dataManager.read(limit).clone();
-                mergeSort.sort(dataCopyMerge);
-
-                Integer[] dataCopyQuick = dataManager.read(limit).clone();
-                quickSort.sort(dataCopyQuick);
-               
-                Integer[] dataCopyRadix = dataManager.read(limit).clone();
-                radixSort.sort(dataCopyRadix);
-
-                Integer[] dataCopyInsertion = dataManager.read(limit).clone();
-                insertionSort.sort(dataCopyInsertion);
+            System.out.println("\nORDENAMIENTO CON ALGORITMOS: ");
+            System.out.print("Ingrese el número de elementos a ordenar: ");
+            String limit = sc.nextLine();
+            System.out.println("\nEl arreglo sera:");
+            System.out.println("1. Desordenado");
+            System.out.println("2. Ordenado");
+            System.out.print("Elige la opción que desees: ");
+            String op = sc.nextLine();
+            try {
+                int limitNumber, opcion;
+                switch (op) {
+                    case "1":
+                        limitNumber = Integer.parseInt(limit);
+                        System.out.println("1. Gnome Sort");
+                        System.out.println("2. Merge Sort");
+                        System.out.println("3. Quick Sort");
+                        System.out.println("4. Radix Sort");
+                        System.out.println("5. Insertion Sort");
+                        System.out.print("Selecciona el algoritmo de ordenamiento:");
+                        opcion = sc.nextInt();
+        
+                        switch (opcion) {
+                            case 1:
+                                Integer[] dataCopy = dataManager.read(limitNumber).clone();
+                                gnomeSort.sort(dataCopy);
+                                System.out.println("Lista ordenada con GnomeSort");
+                                break;
+                            
+                            case 2:
+                                Integer[] dataCopyMerge = dataManager.read(limitNumber).clone();
+                                mergeSort.sort(dataCopyMerge);
+                                System.out.println("Lista ordenada con MergeSort");
+                                break;
+        
+                            case 3:
+                                Integer[] dataCopyQuick = dataManager.read(limitNumber).clone();
+                                quickSort.sort(dataCopyQuick);
+                                System.out.println("Lista ordenada con QuickSort");
+                                break;
+        
+                            case 4:
+                                Integer[] dataCopyRadix = dataManager.read(limitNumber).clone();
+                                radixSort.sort(dataCopyRadix);
+                                System.out.println("Lista ordenada con RadixSort");
+                                break;
+        
+                            case 5:
+                                Integer[] dataCopyInsertion = dataManager.read(limitNumber).clone();
+                                insertionSort.sort(dataCopyInsertion);
+                                System.out.println("Lista ordenada con InsertionSort");
+                                break;
+                            
+                            default:
+                                System.out.println("Opción no válida");
+                                break;
+                        }
+                        break;
+                    case "2":
+                        limitNumber = Integer.parseInt(limit);
+                        System.out.println("1. Gnome Sort");
+                        System.out.println("2. Merge Sort");
+                        System.out.println("3. Quick Sort");
+                        System.out.println("4. Radix Sort");
+                        System.out.println("5. Insertion Sort");
+                        System.out.print("Selecciona el algoritmo de ordenamiento:");
+                        opcion = sc.nextInt();
+        
+                        switch (opcion) {
+                            case 1:
+                                Integer[] dataCopy = dataManager.read(limitNumber).clone();
+                                Integer[] sortedGnome = quickSort.sort(dataCopy);
+                                gnomeSort.sort(sortedGnome);
+                                System.out.println("Lista ordenada con GnomeSort");
+                                break;
+        
+                            case 2:
+                                Integer[] dataCopyMerge = dataManager.read(limitNumber).clone();
+                                Integer[] sortedMerge = quickSort.sort(dataCopyMerge);
+                                mergeSort.sort(sortedMerge);
+                                System.out.println("Lista ordenada con MergeSort");
+                                break;
+        
+                            case 3:
+                                Integer[] dataCopyQuick = dataManager.read(limitNumber).clone();
+                                Integer[] sortedQuick = radixSort.sort(dataCopyQuick);
+                                quickSort.sort(sortedQuick);
+                                System.out.println("Lista ordenada con QuickSort");
+                                break;
+        
+                            case 4:
+                                Integer[] dataCopyRadix = dataManager.read(limitNumber).clone();
+                                Integer[] sortedRadix = quickSort.sort(dataCopyRadix);
+                                radixSort.sort(sortedRadix);
+                                System.out.println("Lista ordenada con RadixSort");
+                                break;
+        
+                            case 5:
+                                Integer[] dataCopyInsertion = dataManager.read(limitNumber).clone();
+                                Integer[] sortedInsertion = quickSort.sort(dataCopyInsertion);
+                                insertionSort.sort(sortedInsertion);
+                                System.out.println("Lista ordenada con InsertionSort");
+                                break;
+        
+                            default:
+                                System.out.println("Opción no válida");
+                                break;
+                        }
+                        break;
                 
-                limit += 200;
+                    default:
+                        System.out.println("Opción no válida");
+                        break;
+                }
+                System.out.println("");
+            } catch (NumberFormatException e) {
+                System.out.println("El valor ingresado no es un número");
+                return;
             }
+
 
         } catch (Exception e) {
             System.err.println("Error al trabajar con el archivo: " + e.getMessage());
--- RadixSort.java
+++ RadixSort.java
@@ -4,7 +4,7 @@
  * @description Clase encargada de usar el algoritmo RedixSort
  * @references https://www.geeksforgeeks.org/radix-sort/
  */
- import java.util.Arrays;
+import java.util.Arrays;
 
 public class RadixSort <T extends Comparable<T>> implements ISort<T>{
 
@@ -13,37 +13,37 @@ public class RadixSort <T extends Comparable<T>> implements ISort<T>{
         if (arr == null || arr.length == 0) {
             return arr;
         }
-        int max = getMax(arr);
-        for (int exp = 1; max / exp > 0; exp *= 10) {
+        Integer max = getMax(arr);
+        for (Integer exp = 1; max / exp > 0; exp *= 10) {
             countingSort(arr, exp);
         }
         return arr;
     }
 
-    private int getMax(T[] arr) {
-        int max = (int) arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if ((int) arr[i] > max) {
-                max = (int) arr[i];
+    private Integer getMax(T[] arr) {
+        Integer max = (Integer) arr[0];
+        for (Integer i = 1; i < arr.length; i++) {
+            if ((Integer) arr[i] > max) {
+                max = (Integer) arr[i];
             }
         }
         return max;
     }
 
-    private void countingSort(T[] arr, int exp) {
-        int n = arr.length;
+    private void countingSort(T[] arr, Integer exp) {
+        Integer n = arr.length;
         T[] output = Arrays.copyOf(arr, n);
-        int[] count = new int[10];
+        Integer[] count = new Integer[10];
         Arrays.fill(count, 0);
-        for (int i = 0; i < n; i++) {
-            count[((int) arr[i]) / exp % 10]++;
+        for (Integer i = 0; i < n; i++) {
+            count[((Integer) arr[i]) / exp % 10]++;
         }
-        for (int i = 1; i < 10; i++) {
+        for (Integer i = 1; i < 10; i++) {
             count[i] += count[i - 1];
         }
-        for (int i = n - 1; i >= 0; i--) {
-            output[count[((int) arr[i]) / exp % 10] - 1] = arr[i];
-            count[((int) arr[i]) / exp % 10]--;
+        for (Integer i = n - 1; i >= 0; i--) {
+            output[count[((Integer) arr[i]) / exp % 10] - 1] = arr[i];
+            count[((Integer) arr[i]) / exp % 10]--;
         }
         System.arraycopy(output, 0, arr, 0, n);
     }
